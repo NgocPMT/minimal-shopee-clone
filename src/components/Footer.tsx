650370e7172eb41da324d511b949669bfbebb006
NgocPMT <149455563+NgocPMT@users.noreply.github.com>
2025-08-10 11:54:54 +0700

    implement footer and style page layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white p-1.5 text-center">
+      &copy; Created by Nathan Pham
+    </footer>
+  );
+};
+
+export default Footer;
--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,12 +1,14 @@
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <>
+    <div className="min-h-dvh grid grid-rows-[auto_1fr_auto]">
       <Navbar />
       <Outlet />
-    </>
+      <Footer />
+    </div>
   );
 };
 
