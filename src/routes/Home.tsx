dc8e741d07308fbc5a7b23cf1cf374498fa7f42e
NgocPMT <149455563+NgocPMT@users.noreply.github.com>
2025-08-10 11:37:57 +0700

    implement hero section with styling

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,8 +1,26 @@
 import Categories from "../components/Categories";
+import heroBackground from "../assets/images/hero-background.jpg";
+import { Link } from "react-router";
 
 const Home = () => {
   return (
-    <div className="bg-gray-50">
+    <div>
+      <section className="relative w-full  text-center">
+        <img
+          src={heroBackground}
+          alt=""
+          className="absolute object-cover w-full h-full -z-10"
+        />
+        <div className="absolute object-cover w-full h-full bg-black/10 -z-10"></div>
+        <h1 className="text-amber-50 text-2xl pt-3 font-bold">Shopeefy</h1>
+        <p className=" text-amber-100">Minimize your shopping experience.</p>
+        <Link
+          to="#"
+          className="bg-amber-700 text-amber-50 p-2 px-3 rounded-md text-sm hover:bg-amber-800 focus:bg-amber-800 inline-block mt-4 mb-5"
+        >
+          Shop Now
+        </Link>
+      </section>
       <Categories />;
     </div>
   );
