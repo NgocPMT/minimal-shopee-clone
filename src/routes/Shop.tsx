97244c3feaa5fadc13ec5a4799030d6d2839f5fc
NgocPMT <149455563+NgocPMT@users.noreply.github.com>
2025-08-11 22:38:13 +0700

    add animation for the filters section

--- src/routes/Shop.tsx
+++ src/routes/Shop.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
 import { Link } from "react-router";
 
@@ -19,7 +19,7 @@ const Shop = () => {
   const [products, setProducts] = useState<Product[] | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
-  const filterModalRef = useRef<HTMLDialogElement | null>(null);
+  const [isFiltersShow, setIsFiltersShow] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -47,100 +47,115 @@ const Shop = () => {
     fetchData();
   }, []);
 
-  const openFilterModal = () => {
-    if (!filterModalRef.current) return;
-    filterModalRef.current.showModal();
+  const openFilters = () => {
+    if (isFiltersShow) return;
+    setIsFiltersShow(true);
   };
 
-  const closeFilterModal = () => {
-    if (!filterModalRef.current) return;
-    filterModalRef.current.close();
+  const closeFilters = () => {
+    if (!isFiltersShow) return;
+    setIsFiltersShow(false);
   };
 
   return (
     <div>
-      <dialog
-        ref={filterModalRef}
-        className="fixed! left-0 w-full max-w-full! m-0 p-0 top-[20vh] min-h-[80vh] bg-white"
-      >
-        <div className="flex justify-between p-2 border-b border-gray-300">
-          <p className="font-semibold">Filters</p>
-          <button onClick={closeFilterModal} className="cursor-pointer">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-5.5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18 18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
-        </div>
-        <div className="p-2">
-          <section className="mb-3">
-            <h5 className="font-semibold mb-3">Price</h5>
-            <div className="flex gap-3 flex-wrap">
-              <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
-                High to Low
-              </button>
-              <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
-                Low to High
+      {isFiltersShow && (
+        <>
+          <div className="fixed left-0 top-0 right-0 bottom-0 bg-black/20 z-10"></div>
+          <div className="fixed z-20 m-0 p-0 bottom-0 left-0 right-0 min-h-[80vh] bg-white animate-push-in-bottom">
+            <div className="flex justify-between p-2 border-b border-gray-300">
+              <p className="font-semibold">Filters</p>
+              <button onClick={closeFilters} className="cursor-pointer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-5.5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18 18 6M6 6l12 12"
+                  />
+                </svg>
               </button>
             </div>
-          </section>
-          <section className="mb-3">
-            <h5 className="font-semibold mb-3">Category</h5>
-            <div className="flex gap-3 flex-wrap">
-              <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
-                Men's Clothes
-              </button>
-              <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
-                Women's Clothes
-              </button>
-              <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
-                Electronics
-              </button>
-              <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
-                Accessories
-              </button>
+            <div className="p-2">
+              <section className="mb-3">
+                <h5 className="font-semibold mb-3">Price</h5>
+                <div className="flex gap-3 flex-wrap">
+                  <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
+                    High to Low
+                  </button>
+                  <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
+                    Low to High
+                  </button>
+                  <form className="flex gap-1 items-center ring ring-gray-300 rounded-md overflow-hidden">
+                    <input
+                      type="number"
+                      placeholder="Min"
+                      className="p-2 text-center"
+                    />
+                    <span className="text-gray-500">---</span>
+                    <input
+                      type="number"
+                      placeholder="Max"
+                      className="p-2 text-center"
+                    />
+                  </form>
+                </div>
+              </section>
+              <section className="mb-3">
+                <h5 className="font-semibold mb-3">Category</h5>
+                <div className="flex gap-3 flex-wrap">
+                  <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
+                    Men's Clothes
+                  </button>
+                  <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
+                    Women's Clothes
+                  </button>
+                  <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
+                    Electronics
+                  </button>
+                  <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
+                    Accessories
+                  </button>
+                </div>
+              </section>
+              <section className="mb-3">
+                <h5 className="font-semibold mb-3">Rating</h5>
+                <div className="flex gap-3 flex-wrap">
+                  <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
+                    5 star
+                  </button>
+                  <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
+                    from 4 star
+                  </button>
+                  <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
+                    from 3 star
+                  </button>
+                  <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
+                    from 2 star
+                  </button>
+                  <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
+                    from 1 star
+                  </button>
+                </div>
+              </section>
+              <div className="border-t border-gray-300 absolute bottom-0 left-0 right-0 flex justify-end gap-2 px-2 py-2">
+                <button className="py-1.5 px-4 bg-white text-amber-700 border border-amber-700 rounded-sm cursor-pointer hover:bg-gray-100">
+                  Reset
+                </button>
+                <button className="py-1.5 px-4 bg-amber-700 text-white rounded-sm cursor-pointer hover:bg-amber-800">
+                  Apply
+                </button>
+              </div>
             </div>
-          </section>
-          <section className="mb-3">
-            <h5 className="font-semibold mb-3">Rating</h5>
-            <div className="flex gap-3 flex-wrap">
-              <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
-                5 star
-              </button>
-              <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
-                from 4 star
-              </button>
-              <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
-                from 3 star
-              </button>
-              <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
-                from 2 star
-              </button>
-              <button className="ring ring-gray-300 hover:ring-amber-700 cursor-pointer rounded-md py-1.5 px-2">
-                from 1 star
-              </button>
-            </div>
-          </section>
-          <div className="border-t border-gray-300 absolute bottom-0 left-0 right-0 flex justify-end gap-2 px-2 py-2">
-            <button className="py-1.5 px-4 bg-white text-amber-700 border border-amber-700 rounded-sm cursor-pointer hover:bg-gray-100">
-              Reset
-            </button>
-            <button className="py-1.5 px-4 bg-amber-700 text-white rounded-sm cursor-pointer hover:bg-amber-800">
-              Apply
-            </button>
           </div>
-        </div>
-      </dialog>
+        </>
+      )}
       <div className="min-h-10 bg-white text-amber-700 flex items-center justify-between px-2 py-2.5">
         <Link
           to="/"
@@ -163,7 +178,7 @@ const Shop = () => {
         </Link>
         <button
           className="relative p-1.5 mr-5 cursor-pointer"
-          onClick={openFilterModal}
+          onClick={openFilters}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -182,7 +197,7 @@ const Shop = () => {
           <span className="absolute bottom-0 -right-4 text-xs">Filter</span>
         </button>
       </div>
-      <div className="grid grid-cols-2 gap-2 p-2">
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-2 p-2">
         {error && <p>{error}</p>}
         {loading && <p>Loading...</p>}
         {products &&
