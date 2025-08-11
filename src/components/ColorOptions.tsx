9e4203348742c6ae223a56393c5d326067d1023f
NgocPMT <149455563+NgocPMT@users.noreply.github.com>
2025-08-11 14:46:31 +0700

    implement color choosing section

---
+++ src/components/ColorOptions.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import ColorOption from "./ColorOption";
+
+type ColorOptionsProps = {
+  colors: string[];
+};
+
+const ColorOptions = ({ colors }: ColorOptionsProps) => {
+  const [activeColor, setActiveColor] = useState<string>(colors[0]);
+
+  const handleActive = (color: string) => setActiveColor(color);
+
+  return (
+    <section className="p-2">
+      <h5 className="font-bold text-amber-700 mb-2 text-lg">Color</h5>
+      <div className="flex gap-3">
+        {colors.map((color) => (
+          <ColorOption
+            key={color}
+            color={color}
+            active={activeColor === color}
+            handleActive={handleActive}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default ColorOptions;
---
+++ src/components/ProductDetails.tsx
@@ -0,0 +1,27 @@
+import ColorOptions from "./ColorOptions";
+
+const ProductDetails = () => {
+  return (
+    <div>
+      <img
+        src="https://blocks.astratic.com/img/general-img-landscape.png"
+        alt=""
+      />
+      <ColorOptions colors={["black", "white", "blue", "pink"]} />
+      <h5>Size</h5>
+      <div>
+        <div>S</div>
+        <div>M</div>
+        <div>L</div>
+      </div>
+      <p>$150.00</p>
+      <h5>Mens Casual Premium Slim Fit T-Shirts</h5>
+      <div>
+        <button>Add To Card</button>
+        <button>Buy Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;
