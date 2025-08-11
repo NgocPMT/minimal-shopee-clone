72cae79f9b42838f5df34c8b3b69cc872d702a1f
NgocPMT <149455563+NgocPMT@users.noreply.github.com>
2025-08-11 10:27:45 +0700

    add the rating display for ProductCard and finish Recommended section on mobile view

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,23 +1,48 @@
 type ProductCardProps = {
-  imgURL?: string;
+  image?: string;
   title: string;
   price: number;
+  rating: { count: number; rate: number };
 };
 
 const ProductCard = ({
-  imgURL = "https://blocks.astratic.com/img/general-img-landscape.png",
+  image = "https://blocks.astratic.com/img/general-img-landscape.png",
   title,
   price,
+  rating,
 }: ProductCardProps) => {
   return (
-    <div className="bg-white w-full border border-gray-300 hover:border-amber-600 hover:-translate-y-0.5 rounded-sm overflow-hidden">
-      <img src={imgURL} alt="" />
-      <div className="p-2">
-        <h5 className="line-clamp-2 text-sm">{title}</h5>
-        <p className="text-amber-700 text-lg pt-2">${price}.00</p>
+    <div className="bg-white w-full border border-gray-300 hover:border-2 hover:border-amber-600 hover:-translate-y-0.5 rounded-md overflow-hidden flex flex-col max-h-72 relative">
+      <img src={image} alt="" className="w-full h-[65%] object-contain " />
+      <div className="p-2 flex flex-col justify-between grow-1">
+        <h5 className="line-clamp-2 text-sm text-black">{title}</h5>
+        <div className="flex justify-between items-center">
+          <p className="text-amber-700 text-lg">${price}</p>
+          <p className="flex items-center text-md text-gray-600">
+            {rating.rate}
+            {starSvg}({rating.count})
+          </p>
+        </div>
       </div>
     </div>
   );
 };
+
+const starSvg = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    className="size-4 text-amber-400"
+  >
+    <path
+      fill="currentColor"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+      d="m8.587 8.236l2.598-5.232a.911.911 0 0 1 1.63 0l2.598 5.232l5.808.844a.902.902 0 0 1 .503 1.542l-4.202 4.07l.992 5.75c.127.738-.653 1.3-1.32.952L12 18.678l-5.195 2.716c-.666.349-1.446-.214-1.319-.953l.992-5.75l-4.202-4.07a.902.902 0 0 1 .503-1.54z"
+    ></path>
+  </svg>
+);
 
 export default ProductCard;
--- src/components/Recommended.tsx
+++ src/components/Recommended.tsx
@@ -1,7 +1,52 @@
 import { Link } from "react-router";
 import ProductCard from "./ProductCard";
+import { useEffect, useState } from "react";
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    count: number;
+    rate: number;
+  };
+}
 
 const Recommended = () => {
+  const [products, setProducts] = useState<Product[] | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const recommendedLength = 4;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products", {
+          mode: "cors",
+        });
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
+        }
+        const data = await response.json();
+        setProducts(data);
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError("unknown error");
+        }
+        setProducts(null);
+      } finally {
+        setLoading(false);
+        setError(null);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <section className="bg-white">
       <div className="flex justify-between px-3 py-2">
@@ -10,24 +55,23 @@ const Recommended = () => {
           More &gt;
         </Link>
       </div>
-      <div className="grid grid-cols-2 gap-2 min-h-50 bg-gray-200 px-1 py-2">
-        <ProductCard
-          title="A Very Cool And Expensive T-Shirt I Guess I Add More Text For No Reason"
-          price={500}
-        />
-        <ProductCard
-          title="A Very Cool And Expensive T-Shirt I Guess I Add More Text For No Reason"
-          price={500}
-        />
-        <ProductCard
-          title="A Very Cool And Expensive T-Shirt I Guess I Add More Text For No Reason"
-          price={500}
-        />
-        <ProductCard
-          title="A Very Cool And Expensive T-Shirt I Guess I Add More Text For No Reason"
-          price={500}
-        />
-      </div>
+      {products && (
+        <div className="grid grid-cols-2 auto-rows-min gap-2 min-h-50 bg-gray-200 px-1 py-2">
+          {products.slice(0, recommendedLength).map((product) => (
+            <ProductCard
+              key={product.id}
+              image={product.image}
+              title={product.title}
+              price={product.price}
+              rating={product.rating}
+            />
+          ))}
+        </div>
+      )}
+
+      {loading && <p>Loading...</p>}
+
+      {error && <p className="text-red-500">{error}</p>}
     </section>
   );
 };
