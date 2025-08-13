ee172955642af227382ac1bebc50fab158be5e6b
NgocPMT <149455563+NgocPMT@users.noreply.github.com>
2025-08-13 15:47:10 +0700

    fix grammar and apply transition duration for better UX

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -19,9 +19,9 @@ const ProductCard = ({
   return (
     <Link
       to={`/products/${id}`}
-      className="bg-white w-full max-w-80 border border-gray-300 hover:border-2 hover:border-amber-600 hover:-translate-y-0.5 rounded-md overflow-hidden flex flex-col max-h-72"
+      className="bg-white duration-150 w-full max-w-80 border border-gray-300 hover:border-2 hover:border-amber-600 hover:-translate-y-0.5 rounded-md overflow-hidden flex flex-col max-h-72"
     >
-      <img src={image} alt="" className="w-full h-[65%] object-contain " />
+      <img src={image} alt="" className="w-full h-[65%] object-contain" />
       <div className="p-2 flex flex-col justify-between grow-1">
         <h5 className="line-clamp-2 text-sm text-black sm:text-md">{title}</h5>
         <div className="flex justify-between items-center">
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -14,7 +14,7 @@ const Home = () => {
         />
         <div className="absolute object-cover w-full h-full bg-black/10 -z-10"></div>
         <h1 className="text-amber-50 text-2xl pt-3 font-bold">Shopeefy</h1>
-        <p className=" text-amber-100">Minimize your shopping experience.</p>
+        <p className=" text-amber-100">Simplify your shopping experience.</p>
         <Link
           to="shop"
           className="bg-amber-700 text-amber-50 p-2 px-3 rounded-md text-sm hover:bg-amber-800 focus:bg-amber-800 inline-block mt-4 mb-5"
