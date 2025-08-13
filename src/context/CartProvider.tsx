83b26e3a2ad924db3b595ee40c07c523b33b3652
NgocPMT <149455563+NgocPMT@users.noreply.github.com>
2025-08-13 16:38:04 +0700

    remove the initial items from the cartItems

--- src/context/CartProvider.tsx
+++ src/context/CartProvider.tsx
@@ -12,24 +12,7 @@ interface CartProduct {
 }
 
 const CartProvider = ({ children }: { children: ReactNode }) => {
-  const [cartItems, setCartItems] = useState<CartProduct[]>([
-    {
-      id: 1,
-      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
-      title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-      variants: ["XS", "Black"],
-      quantity: 1,
-      price: 109.95,
-    },
-    {
-      id: 2,
-      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
-      title: "Fjallraven - Foldsack No. 2 Backpack, Fits 15 Laptops",
-      variants: ["XS", "Pink"],
-      quantity: 1,
-      price: 99.95,
-    },
-  ]);
+  const [cartItems, setCartItems] = useState<CartProduct[]>([]);
 
   const cart = useMemo(
     () => ({
