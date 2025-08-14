9868abff13383d2fd1f0c11140ad4379352bbcdb
NgocPMT <149455563+NgocPMT@users.noreply.github.com>
2025-08-14 16:43:10 +0700

    limit Product Detail image width for a more consistent look

--- src/routes/ProductDetails.tsx
+++ src/routes/ProductDetails.tsx
@@ -188,7 +188,7 @@ const ProductDetails = () => {
               "https://blocks.astratic.com/img/general-img-landscape.png"
             }
             alt=""
-            className="max-h-[27.5rem] object-contain md:"
+            className="max-h-[27.5rem] max-w-96 object-contain"
           />
           <div className="p-2 mb-6 md:max-w-[30rem] flex flex-col items-start">
             <h5 className="text-black text-xl mb-5 md:font-bold md:mb-2">
